69717e64a1e18c0c68e5219345b54f2a631ddaa9
Holly Tunison <hollytunison@gmail.com>
2023-02-18 16:08:21 -0500

    added chatbox

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,6 +1,27 @@
+import Message from "./Message";
+
 const ChatBox = () => {
+  //hardcode messages
+  const messages = [
+    {
+      id: 1,
+      text: "howdy there",
+      name: "Holly"
+    },
+    {
+      id: 2,
+      text: "hey there",
+      name: "Obi"
+    }
+  ]
+
   return (
-    <div>ChatBox</div>
-  )
+  <div className='pb-44 pt-20 containerWrap'>
+  {messages.map(message => (
+    <Message key={message.id} message={message} />
+  ))}
+  </div>
+)
 }
-export default ChatBox
+
+export default ChatBox;
