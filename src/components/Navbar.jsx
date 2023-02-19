2e78099fe45e2bc26d9d0554e9b50bbfc9f10ee0
Holly Tunison <hollytunison@gmail.com>
2023-02-19 16:55:22 -0500

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ export const Navbar = () => {
   }
 
 	return (
-    <div className="navbar bg-neutral text-neutral-content">
+    <div className="navbar fixed z-10 bg-neutral text-neutral-content">
     <div className="containerWrap flex justify-between">
       <a className="btn btn-ghost normal-case text-xl">instantChat</a>
       {currentUser ? <button onClick={handleLogout}>Logout</button> : ""}
